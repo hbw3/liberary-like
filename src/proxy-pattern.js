89233d2e528aa703f368381a4cb6f756f006eee6
hbw3 <58630828+hbw3@users.noreply.github.com>
2020-02-08 19:15:15 +0530

    proxy-pattern

--- src/proxy-pattern.js
+++ src/proxy-pattern.js
@@ -1,7 +1,7 @@
 //proxy design pattern
 function cryptoCurrency() {
-  console.log("api getting value");
   this.getValue = coin => {
+    console.log(`getting ${coin} value`);
     switch (coin) {
       case "Bitcoin":
         return "$800";
@@ -17,4 +17,33 @@ function cryptoCurrency() {
 
 const cryptoApi = new cryptoCurrency();
 
-console.log(cryptoApi.getValue("Bitcoin"));
+/* console.log(cryptoApi.getValue("Bitcoin"));
+console.log(cryptoApi.getValue("Litecoin"));
+console.log(cryptoApi.getValue("Ethereum")); */
+
+function proxyCryptoCurrency() {
+  this.cache = {};
+  this.getValue = coin => {
+    if (this.cache[coin] == null) {
+      this.cache[coin] = cryptoApi.getValue(coin);
+    }
+
+    return this.cache[coin];
+  };
+}
+
+const proxy = new proxyCryptoCurrency();
+
+console.log(proxy.getValue("Bitcoin"));
+console.log(proxy.getValue("Litecoin"));
+console.log(proxy.getValue("Ethereum"));
+console.log(proxy.getValue("Ethereum"));
+console.log(proxy.getValue("Ethereum"));
+console.log(proxy.getValue("Litecoin"));
+console.log(proxy.getValue("Litecoin"));
+console.log(proxy.getValue("Bitcoin"));
+console.log(proxy.getValue("Bitcoin"));
+console.log(proxy.getValue("Bitcoin"));
+console.log(proxy.getValue("Bitcoin"));
+console.log(proxy.getValue("Bitcoin"));
+console.log(proxy.getValue("Bitcoin"));
